2f3831afa8862454b34c6d5a2db1728a8cdfdfc5
henriksveen <simen.henriksveen@gmail.com>
2023-10-02 17:55:43 +0200

    java: advent day1

---
+++ java/advent2022/src/main/java/loom/learning/Day1.java
@@ -0,0 +1,61 @@
+package loom.learning;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.stream.LongStream;
+
+public class Day1 {
+    public static void main(String[] args) {
+        new Day1().run();
+    }
+
+    private void run() {
+        System.out.println("Part 1: " + part1());
+        System.out.println("Part 2: " + part2());
+    }
+
+    private Long part1() {
+        return input().max().getAsLong();
+    }
+
+    private Long part2() {
+        long[] nums = input().sorted().toArray();
+        return nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3];
+    }
+
+    private static LongStream input() {
+        return Arrays.stream(
+                inputDay1().split("\n\n")).mapToLong(
+                elf -> elf.lines().mapToLong(Long::parseLong).sum()
+        );
+    }
+
+    private static String inputDay1() {
+        try {
+            return Files.readString(new File("/home/simen/dev/code/learning-loom/java/advent2022/src/main/resources/day1.txt").toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String inputTest() {
+        return """
+                1000
+                2000
+                3000
+                                
+                4000
+                                
+                5000
+                6000
+                                
+                7000
+                8000
+                9000
+                                
+                10000
+                """;
+    }
+}
